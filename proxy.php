8e386c795f2c28dcd5223052f755170d8b0c8d51
khaled1192 <k2555066@gmail.com>
2025-05-24 05:45:28 +0300

    proxy.php

--- proxy.php
+++ proxy.php
@@ -5,10 +5,23 @@ header("Access-Control-Allow-Origin: *");
 function fetch($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
+    
+    // نجعل الطلب يشبه المتصفح
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Accept: application/json",
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "Connection: keep-alive"
+    ]);
+    
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0");
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
     $response = curl_exec($ch);
+
+    // لمعرفة إذا فيه خطأ
+    if (curl_errno($ch)) {
+        echo "cURL error: " . curl_error($ch);
+    }
+
     curl_close($ch);
     return $response;
 }
@@ -16,8 +29,12 @@ function fetch($url) {
 $info = fetch("http://176.97.212.148:30120/info.json");
 $players = fetch("http://176.97.212.148:30120/players.json");
 
-echo "--- info.json ---\n";
-var_dump($info);
-
-echo "\n\n--- players.json ---\n";
-var_dump($players);
+if ($info === false || $players === false) {
+    http_response_code(500);
+    echo json_encode(["error" => "لا يمكن الاتصال بسيرفر FiveM"]);
+} else {
+    echo json_encode([
+        "info" => json_decode($info, true),
+        "players" => json_decode($players, true)
+    ]);
+}
