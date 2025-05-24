1cd1bf0cccbbe5cd6f7d378c91b3c6e1b7495015
khaled1192 <k2555066@gmail.com>
2025-05-24 05:30:38 +0300

    proxy.php

--- proxy.php
+++ proxy.php
@@ -2,11 +2,15 @@
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
-$data = @file_get_contents("http://176.97.212.148:30120/players.json");
+$info = @file_get_contents("http://176.97.212.148:30120/info.json");
+$players = @file_get_contents("http://176.97.212.148:30120/players.json");
 
-if ($data === FALSE) {
+if ($info === FALSE || $players === FALSE) {
     http_response_code(500);
     echo json_encode(["error" => "لا يمكن الاتصال بسيرفر FiveM"]);
 } else {
-    echo $data;
+    echo json_encode([
+        "info" => json_decode($info, true),
+        "players" => json_decode($players, true)
+    ]);
 }
